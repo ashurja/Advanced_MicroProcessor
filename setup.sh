3e57ba07205ee2dc157a3b00f65998a699b60ebb
ashurja <jamshed00ashurov@gmail.com>
2022-07-05 17:00:19 -0700

    negedge cache bank

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-if [ $1 == "0" ]; then
-    export export CSE148_TOOLS='/software/CSE/cs148sp22/tools'
-    env | grep '^CSE148_TOOLS='
-else 
-    source /Users/jamshedashurov/Desktop/CSE148/CSE148/tools/oss-cad-suite/environment
-    export CSE148_TOOLS="/Users/jamshedashurov/Desktop/CSE148/CSE148/tools"
-fi
+source /Users/jamshedashurov/Desktop/CSE148/tools/oss-cad-suite/environment
+export CSE148_TOOLS="/Users/jamshedashurov/Desktop/CSE148/tools"
